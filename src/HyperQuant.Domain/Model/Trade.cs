98d52d32c2cadbf4b83dcaf2b2465ed0ea3918cf
zhingelvadim <zhingelvadim@gmail.com>
2025-03-21 20:01:41 +0300

    feat (Solution): - create domain layer

---
+++ src/HyperQuant.Domain/Model/Trade.cs
@@ -0,0 +1,35 @@
+﻿namespace HyperQuant.Domain.Model
+{
+    public class Trade
+    {
+        /// <summary>
+        /// Id трейда
+        /// </summary>
+        public string Id { get; set; } = null!;
+
+        /// <summary>
+        /// Валютная пара
+        /// </summary>
+        public string Pair { get; set; } = null!;
+
+        /// <summary>
+        /// Цена трейда
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Объем трейда
+        /// </summary>
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        /// Направление (buy/sell)
+        /// </summary>
+        public string Side { get; set; } = null!;
+
+        /// <summary>
+        /// Время трейда
+        /// </summary>
+        public DateTimeOffset Time { get; set; }
+    }
+}
